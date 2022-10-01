a232e197c9773b137969ec5e2be5b4d056d829be
Luis del Valle <luis.delvalle0124@gmail.com>
2022-10-01 13:35:30 -0700

    changed name for divs

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let gameBoard = document.querySelector('.gameBoard');
-// let block = document.querySelector('div');
 
 //16x16 board 
 for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    gameBoard.appendChild(div);
+    const block = document.createElement('div');
+    gameBoard.appendChild(block);
 }
