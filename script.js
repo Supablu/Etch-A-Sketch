d2e676b1d16504575ab8aa7b0a7857fd91121942
Luis del Valle <luis.delvalle0124@gmail.com>
2022-10-01 09:50:47 -0700

    Added 16x16 grid

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 let gameBoard = document.querySelector('.gameBoard');
+// let block = document.querySelector('div');
+
+//16x16 board 
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    gameBoard.appendChild(div);
+}
