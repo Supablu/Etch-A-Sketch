2f3c7a7c7c207f3230fa7afec0904c898712d793
Luis del Valle <luis.delvalle0124@gmail.com>
2022-10-01 19:42:42 -0700

    Added clear button in html. Block divs fill in black when hovered over via JS

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 let gameBoard = document.querySelector('.gameBoard');
+let clear = document.querySelector('#clear');
 
 //16x16 board 
 for (let i = 0; i < 256; i++) {
     const block = document.createElement('div');
+    block.id = 'block';
     gameBoard.appendChild(block);
 }
+
+//When pointer hovers over a block, fill in with color black
+let colorBlack = document.querySelectorAll('#block');
+for (const color of colorBlack) {
+    color.addEventListener('mouseover', () => {
+        color.style.backgroundColor = 'black';
+    });
+}
+
+//Clear button to remove all div element colors
+
